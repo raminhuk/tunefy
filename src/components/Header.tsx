d47b46e8b1c6d8375f183ed1f586037762c4e1fb
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-09-13 21:24:59 -0300

    feat: 🎸 Adicionando tag de link next

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Nunito_Sans } from 'next/font/google';
 import Image from 'next/image';
 import React from 'react';
 import User from './User';
+import Link from 'next/link';
 
 
 const nunito = Nunito_Sans({
@@ -18,17 +19,17 @@ export function Header() {
         <header className="text-white">
             <div className="max-w-7xl w-11/12 mx-auto flex justify-between items-center">
                 <div className="flex items-center h-auto py-3">
-                    <a href="/" className="flex items-center gap-2">
+                    <Link href="/" className="flex items-center gap-2">
                         <h1 className={`text-transparent bg-gradient-to-r from-customPink via-customPink2 to-customBlue bg-clip-text font-extrabold text-2xl tracking-widest ${nunito.className}`}>TUNEFY</h1>
                         <Image width={64} height={31} className="h-auto w-16" src="../tunefy-logo.svg" alt="Logo Tunefy" />
-                    </a>
+                    </Link>
                 </div>
                 <nav className="flex">
-                    <a href="/" className="px-6 py-8 hover:text-gray-400">Home</a>
-                    <a href="/tracks" className="px-6 py-8 hover:text-gray-400">Top Tracks</a>
-                    <a href="/artists" className="px-6 py-8 hover:text-gray-400">Top Artists</a>
-                    <a href="/genres" className="px-6 py-8 hover:text-gray-400">Top Genres</a>
-                    <a href="/recently" className="px-6 py-8 hover:text-gray-400">Recently Played</a>
+                    <Link href="/" className="px-6 py-8 hover:text-gray-400">Home</Link>
+                    <Link href="/tracks" className="px-6 py-8 hover:text-gray-400">Top Tracks</Link>
+                    <Link href="/artists" className="px-6 py-8 hover:text-gray-400">Top Artists</Link>
+                    <Link href="/genres" className="px-6 py-8 hover:text-gray-400">Top Genres</Link>
+                    <Link href="/recently" className="px-6 py-8 hover:text-gray-400">Recently Played</Link>
 
                 </nav>
 
