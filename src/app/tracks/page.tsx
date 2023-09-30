0343d6ece36732a7950fedb1c277b1ed09fe8422
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-09-30 16:49:15 -0300

    style: 💄 Novo layout para listagem das musicas

--- src/app/tracks/page.tsx
+++ src/app/tracks/page.tsx
@@ -118,15 +118,13 @@ export default function Tracks() {
 
     return (
         <div>
+            <div className="w-full" style={idTrack !== null ? { display: 'block' } : { display: 'none' }}>
+                <div className="w-full flex flex-col fixed -bottom-5 left-0">
+                    <div className="bg-gray-800 h-20 w-full flex" id="embed-iframe"></div>
+                </div>
+            </div>
             {topTracks ? (
                 <>
-                <div>
-                    <div className="w-full" style={idTrack !== null ? { display: 'block' } : { display: 'none' }}>
-                        <div className="w-full flex flex-col fixed -bottom-5 left-0">
-                            <div className="bg-gray-800 h-20 w-full flex" id="embed-iframe"></div>
-                        </div>
-                    </div>
-                </div>
                 <div className="max-w-4xl w-11/12 mx-auto mt-8">
                     <CreatePlaylist timeRange={timeRange} play={handleTrack}/>
                     <div className="text-center max-w-7xl mx-auto">
@@ -155,24 +153,29 @@ export default function Tracks() {
                    
                     {timeRanges.map((time) => (
                         <div key={time} className="none" style={time === timeRange ? { display: 'block' } : { display: 'none' }}>
-                            <ul>
-                                {topTracks?.[time]?.map((track: Track) => (
-                                    <li key={track.id} className="p-3 rounded-md flex space-y-1 justify-between items-center gap-4 bg-zinc-900 my-2">
-                                        <div className="flex gap-4 items-center">
-                                            <Image className="h-auto rounded-sm" style={{ maxWidth: '60px' }} alt={track.artists[0].name} src={track.album.images[0].url} width={track.album.images[0].width} height={track.album.images[0].height} />
-                                            <div className="flex flex-col">
-                                                <span className="text-gray-100 font-semibold text-xs tracking-wider lg:text-base">{track.name}</span>
-                                                <span className="text-gray-400 text-sm">{track.artists[0].name}</span>
+                            <ul className="flex flex-wrap">
+                                {topTracks?.[time]?.map((track: Track, index: number) => (
+                                    <>
+                                    <li key={track.id} className={`w-full items-center flex gap-3 ${index < 3 ? 'flex-1 bg-inherit flex-col' : 'flex'} ${index === 1 ? 'order-1 mt-10' : 'order-5'} ${index === 2 ? 'mt-20' : ''}`}>
+                                        <span className='font-semibold text-2xl tracking-wider'>{index+1}°</span>
+                                        <div className={`w-full p-3 rounded-md flex space-y-1 justify-between items-center gap-4 my-1 ${index < 3 ? 'bg-inherit flex-col' : 'bg-zinc-900'}`}>
+                                            <div className={`flex gap-4 items-center ${index < 3 && 'flex-col text-center'}`}>
+                                                <Image className={`h-auto ${index < 3 ? 'rounded-full' : 'rounded-sm'}`} style={{ maxWidth: `${index < 3 ? '160px' : '80px'}` }} alt={track.artists[0].name} src={track.album.images[0].url} width={track.album.images[0].width} height={track.album.images[0].height} />
+                                                <div className="flex flex-col">
+                                                    <span className="text-gray-100 font-semibold text-xs tracking-wider lg:text-base">{track.name}</span>
+                                                    <span className="text-gray-400 text-sm">{track.artists[0].name}</span>
+                                                </div>
                                             </div>
+                                            <button onClick={() => { handleTrack(`spotify:track:${track.id}`) }}>
+                                                {idTrack === `spotify:track:${track.id}` && isPlay ? (
+                                                    <BsPauseCircleFill size={30} />
+                                                ) : (
+                                                    <BsPlayCircleFill size={30} />
+                                                )}
+                                            </button>
                                         </div>
-                                        <button onClick={() => { handleTrack(`spotify:track:${track.id}`) }}>
-                                            {idTrack === track.id && isPlay ? (
-                                                <BsPauseCircleFill size={30} />
-                                            ) : (
-                                                <BsPlayCircleFill size={30} />
-                                            )}
-                                        </button>
                                     </li>
+                                    </>
                                 ))}
                             </ul>
                         </div>
