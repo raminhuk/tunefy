583cfa42a9b0a0e34be3ebc690326c8a0d8d6ad6
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-10-02 21:14:01 -0300

    style: 💄 Layout topList

--- src/components/TopList.tsx
+++ src/components/TopList.tsx
@@ -15,16 +15,16 @@ export function TopList({ listItems, time, handleTrack, idTrack, isPlay }:TopLis
         <ul className="flex flex-wrap my-10">
             {listItems?.[time]?.map((track: Track, index: number) => (
                 <li key={track.id} className={`w-full items-center flex gap-3 ${index < 3 ? 'flex-1 bg-inherit flex-col' : 'flex'} ${index === 1 ? 'order-1 mt-16' : 'order-5'} ${index === 2 ? 'mt-20' : ''}`}>
+                    <div className={`relative w-full p-4 rounded-md flex space-y-1 justify-between items-center gap-4 my-1.5 ${index < 3 ? 'bg-inherit flex-col' : 'bg-zinc-900'}`}>
                     {index > 2 && (
-                        <span className='font-semibold text-xl tracking-wider'>{index + 1}°</span>
+                        <span className='z-10 w-8 h-8 flex items-center justify-center rounded-full bg-gradient-to-r from-customPink to-customBlue absolute top-2 left-2 font-semibold text-sm tracking-wider'>{index + 1}°</span>
                     )}
-                    <div className={`w-full p-3 rounded-md flex space-y-1 justify-between items-center gap-4 my-1 ${index < 3 ? 'bg-inherit flex-col' : 'bg-zinc-900'}`}>
                         <div className={`flex gap-4 items-center ${index < 3 && 'flex-col text-center'}`}>
                             <div className={`${index < 3 ? "flex justify-center rounded-full relative bg-gradient-to-r from-customPink to-customBlue p-1" : "relative"}`}>
                                 {index < 3 && (
                                     <>
                                         <span className='z-10 w-10 h-10 flex items-center justify-center rounded-full bg-gradient-to-r from-customPink to-customBlue absolute -top-4 font-semibold text-xl tracking-wider'>
-                                        {index + 1}
+                                        {index + 1}°
                                         </span>
                                         {index === 0 && (
                                             <span className="rotate-45 absolute -top-2 -right-4">
@@ -40,7 +40,7 @@ export function TopList({ listItems, time, handleTrack, idTrack, isPlay }:TopLis
                                         </button>
                                     </>
                                 )}
-                                <Image className={`h-auto ${index < 3 ? 'rounded-full' : 'rounded-sm'}`} style={{ maxWidth: `${index < 3 ? '180px' : '80px'}` }} alt={track.artists[0].name} src={track.album.images[0].url} width={track.album.images[0].width} height={track.album.images[0].height} />
+                                <Image className={`h-auto w-full ${index < 3 ? 'rounded-full' : 'rounded-sm'}`} style={{ maxWidth: `${index < 3 ? '180px' : '80px'}` }} alt={track.artists[0].name} src={track.album.images[0].url} width={track.album.images[0].width} height={track.album.images[0].height} />
                             </div>
                             <div className="flex flex-col">
                                 <span className="text-gray-100 font-semibold text-xs tracking-wider lg:text-base">{track.name}</span>
