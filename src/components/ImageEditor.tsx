f8f329d2014e062049029b4cce5232279cbd464b
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-10-03 22:18:23 -0300

    feat: 🎸 Criando modal para imagem compartilhamento

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -1,28 +1,10 @@
 import React, { useState } from 'react';
 import html2canvas from 'html2canvas';
+import Modal from './Modal';
 
 function ImageEditor() {
-  const [text, setText] = useState<string>('');
-  const [imageUrl, setImageUrl] = useState<string>('');
   const [generatedImageUrl, setGeneratedImageUrl] = useState<string>('');
-
-  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
-  };
-
-  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        setImageUrl(event.target?.result as string);
-      };
-
-      reader.readAsDataURL(file);
-    }
-  };
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
   const generateImage = () => {
     const imageContainer = document.getElementById('image-container');
@@ -31,50 +13,26 @@ function ImageEditor() {
       html2canvas(imageContainer).then((canvas) => {
         const generatedImage = canvas.toDataURL('image/png');
         setGeneratedImageUrl(generatedImage);
+        setOpenModal(true);
       });
     }
   };
 
-  const shareOnWhatsApp = () => {
-    if (generatedImageUrl) {
-      const shareText = 'Confira esta imagem:';
-      const whatsappUrl = `whatsapp://send?phone=5547996604764text=${encodeURIComponent(
-        `${shareText} ${window.location.href}`
-      )}&image=${encodeURIComponent(generatedImageUrl)}`;
-
-      console.log(whatsappUrl)
-      window.location.href = whatsappUrl;
-    }
-  };
+  const onClose = () => {
+    setOpenModal(false);
+  }
 
   return (
     <div>
-      <h2>Editor de Imagem</h2>
-      <input type="file" onChange={handleImageUpload} />
-      <br />
-      <textarea
-        placeholder="Insira seu texto aqui"
-        value={text}
-        onChange={handleTextChange}
-      />
-      <br />
-      <button onClick={generateImage}>Gerar Imagem</button>
-      <br />
-      {imageUrl && (
-        <div id="image-container" className='relative'>
-            <span className='absolute top-0 bottom-0'>{text}</span>
-            <img src={imageUrl} alt="Imagem de fundo" />
+        <Modal open={openModal} onClose={onClose}>
+            {generatedImageUrl && <img src={generatedImageUrl} alt="Imagem Gerada" />}
+        </Modal>
+        <div id="image-container" className='w-80 h-80 bg-indigo-500'>
+            Chama
         </div>
-      )}
-      {generatedImageUrl && (
-        <img src={generatedImageUrl} alt="Imagem gerada" style={{ marginTop: '20px' }} />
-      )}
+          
+        <button onClick={generateImage}>Gerar Imagem</button>
 
-<div>
-          <img src={generatedImageUrl} alt="Imagem gerada" style={{ marginTop: '20px' }} />
-          <br />
-          <button onClick={shareOnWhatsApp}>Compartilhar no WhatsApp</button>
-        </div>
     </div>
   );
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,32 @@
+import { FaRegTimesCircle } from "react-icons/fa";
+
+
+interface PopupProps {
+  open: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<PopupProps> = ({ open, onClose, children }) => {
+
+  return (
+    <>
+      {open && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div onClick={onClose} className="absolute inset-0 bg-black opacity-80"></div>
+          <div className="z-10 bg-white rounded-lg p-4 shadow-md">
+            <button
+              className="absolute top-0 right-0 p-5 text-white hover:text-gray-400"
+              onClick={onClose}
+            >
+             <FaRegTimesCircle size={32}/>
+            </button>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
