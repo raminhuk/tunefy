b023984f46504ceccaa17f2640b241df86a522c1
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-10-10 20:50:43 -0300

    style: 💄 ajustes responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import { ToastContainer, toast } from 'react-toastify';
 export default function Home() {
     return (
         <div className="max-w-7xl w-11/12 mx-auto">
-            <div className="min-h-[calc(100vh_-_88px)] bg-[url('/phone3.png')] bg-no-repeat bg-right flex flex-col justify-center">
+            <div className="min-h-[calc(100vh_-_88px)] bg-[url('/phone3.png')] lg:bg-cover bg-contain bg-no-repeat lg:bg-right bg-[40px] flex flex-col justify-center">
                 <h1>
-                    <span className="text-4xl font-semi tracking-wide">As Mais Ouvidas por<br /> Você no Spotify!</span>
+                    <span className="lg:text-4xl text-lg font-semi tracking-wide">As Mais Ouvidas por<br /> Você no Spotify!</span>
                     
                 </h1>
                 <span className="mt-4">
