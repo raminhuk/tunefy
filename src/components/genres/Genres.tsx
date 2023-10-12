02f37bda62f6cd94f3641d6f86225b266ca610df
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-10-12 00:30:32 -0300

    feat: 🎸 Criando componente de genres

--- src/components/genres/Genres.tsx
+++ src/components/genres/Genres.tsx
@@ -1,8 +1,46 @@
 'use client';
 
+import { useMemo } from "react";
+import { useArtistStore } from "../../store/artistStore";
 
 export default function Genres() {
+    const { topArtist } = useArtistStore();
+    const timeRanges: string[] = useMemo(() => ['short_term', 'medium_term', 'long_term'], []);
+    const valueCounts: Record<string, number> = {};
+
+    if (topArtist) {
+        const groupGenres = topArtist.short_term.map((item:any) => {
+            return item.genres
+        })
+
+        const allValues: string[] = [].concat(...groupGenres);
+        allValues.forEach(value => {
+            valueCounts[value] = (valueCounts[value] || 0) + 1;
+        });
+
+        
+        console.log(groupGenres);
+    }
+
+    const valueObjects: { value: string, frequency: number }[] = Object.keys(valueCounts).map(value => ({
+        value,
+        frequency: valueCounts[value]
+      }));
+
+      valueObjects.sort((a, b) => {
+        const countDiff = b.frequency - a.frequency;
+        if (countDiff === 0) {
+          return a.value.localeCompare(b.value);
+        }
+        return countDiff;
+      });
+
+
     return (
-        <>Genero</>
+        <>
+            {valueObjects.map((item, index) => (
+                <span key={index} className="flex column">{item.value} - {item.frequency}</span>
+            ))}
+        </>
     )
 }
