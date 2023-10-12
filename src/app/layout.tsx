3e4491d7130f52e5530aeb56b263829944bbb6a0
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-10-11 22:57:24 -0300

    feat: 🎸 Removendo cookies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Poppins } from 'next/font/google'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import GoogleAnalytics from '../components/GoogleAnalytics'
-import { CookieBanner } from '../components/CookieBanner'
+// import { CookieBanner } from '../components/CookieBanner'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -37,7 +37,7 @@ export default function RootLayout({
                     {children}
                 </div>
                 <Footer/>
-                <CookieBanner/>
+                {/* <CookieBanner/> */}
             </body>
         </html>
     )
