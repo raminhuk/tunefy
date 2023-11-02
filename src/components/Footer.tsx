ec1ee867311929b3860748421c5381e83f4795dd
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-11-02 13:21:28 -0300

    refactor: 💡 new footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,24 @@
+import { Nunito_Sans } from 'next/font/google';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+const nunito = Nunito_Sans({
+    weight: ['200', '400', '600', '800'],
+    subsets: ['latin'],
+})
+
 export function Footer() {
     return (
-        <footer className="bg-neutral-950 text-white py-8">
-            <div className="container mx-auto flex flex-col items-center">
-                <ul className="flex space-x-2 flex-wrap">
+        
+    <footer className="bg-gray-900 shadow mt-10">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <Link href="/" className="flex items-center gap-2">
+                    <h1 className={`text-transparent bg-gradient-to-r from-customPink via-customPink2 to-customBlue bg-clip-text font-extrabold text-2xl tracking-widest ${nunito.className}`}>TUNEFY</h1>
+                    <Image width={64} height={31} className="h-auto w-16" src="../tunefy-logo.svg" alt="Logo Tunefy" />
+                </Link>
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                     <li>
                         <Link href="/" className="lg:px-6 py-8 px-4 hover:text-gray-400">Quem Somos</Link>
                     </li>
@@ -19,11 +32,13 @@ export function Footer() {
                         <Link href="/" className="lg:px-6 py-8 px-4 hover:text-gray-400">Contato</Link>
                     </li>
                 </ul>
-                <div className="mt-4 flex space-x-4">
-                    
-                </div>
             </div>
-        </footer>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-800 lg:my-8" />
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">TuneFy</Link>. All Rights Reserved.</span>
+        </div>
+    </footer>
+
+
     );
 };
 
